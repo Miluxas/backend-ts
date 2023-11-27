29f0b59cc4b2e9b41261f305bccfab783d3efe4d
Milad <milad.ashoori@gmail.com>
2023-11-27 12:42:19 +0330

    update media test

--- src/media/tests/media-crud.e2e-spec.ts
+++ src/media/tests/media-crud.e2e-spec.ts
@@ -6,16 +6,32 @@ describe(' Media CRUD ', () => {
 
   afterAll(baseAfterAll);
 
-  let mediaId;
+  let userToken;
+  it(' User login', async () => {
+    return agent(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: 'user@test.com',
+        password:'123123'
+      })
+      .expect(201)
+      .then((result) => {
+        const resultObject = JSON.parse(result.text);
+        expect(resultObject.payload.user.firstName).toEqual('user');
+        expect(resultObject.payload.user.password).toBeUndefined();
+        userToken=resultObject.payload.token;
+      });
+  });
+
   it(' Media create', async () => {
     return agent(app.getHttpServer())
       .post('/medias')
+      .set('Authorization', `Bearer ${userToken}`)
       .attach('file', './test/nob.jpg')
       .expect(201)
       .then((result) => {
         const resultObject = JSON.parse(result.text);
         expect(resultObject.payload.originalName).toEqual('nob.jpg');
-        mediaId = resultObject.payload.id;
       });
   });
   
